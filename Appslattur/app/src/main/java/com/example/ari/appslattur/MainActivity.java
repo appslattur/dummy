c64a457bc96a60d0994b8ceff245e562dc3c56e0
arifreyrg <arifreyr@gmail.com>
2015-01-18 21:31:25 +0000

    Fixed limited Notification button functionality.

--- Appslattur/app/src/main/java/com/example/ari/appslattur/MainActivity.java
+++ Appslattur/app/src/main/java/com/example/ari/appslattur/MainActivity.java
@@ -167,6 +167,18 @@ public class MainActivity extends Activity {
                 myLocations.clearTable();
             }
         });
+
+        findViewById(R.id.noti).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationHandler nHandler = new NotificationHandler(getApplicationContext(),
+                        "Random TrickTitle",
+                        "Random Title",
+                        "Random Text",
+                        R.drawable.ic_launcher);
+                nHandler.sendNotification();
+            }
+        });
     }
     private void toggleSetRange(){
         setContentView(R.layout.searchrange);
