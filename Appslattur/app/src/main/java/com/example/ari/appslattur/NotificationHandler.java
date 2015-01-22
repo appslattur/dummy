329a12d9747235f68d872444c92e3676f075023e
ArnJonsson <arj26@hi.is>
2015-01-22 16:18:51 +0000

    Classes to handle the Notification service

--- Appslattur/app/src/main/java/com/example/ari/appslattur/NotificationHandler.java
+++ Appslattur/app/src/main/java/com/example/ari/appslattur/NotificationHandler.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 /**
@@ -12,33 +13,77 @@ import android.support.v4.app.NotificationCompat;
  */
 public class NotificationHandler {
 
-    private NotificationCompat.Builder nBuilder;
-    private Context context;
 
-    public NotificationHandler(Context context, String tickerTitle, String title, String text, int icon) {
+     private NotificationCompat.Builder nBuilder;
+        private Context context;
+        private Uri soundAlert;
+        private Intent startingIntent;
+        private boolean vibrate;
 
-        this.context = context;
+        private boolean soundFlag;
 
-        nBuilder = new NotificationCompat.Builder(this.context);
+        public NotificationHandler(Context context, String tickerTitle, String title, String text,
+                                   int icon, Uri uri, Intent startingIntent, boolean vibrate, Uri soundAlert,
+                                   boolean autoCancel) {
 
-        nBuilder.setTicker(tickerTitle);
-        nBuilder.setContentTitle(title);
-        nBuilder.setContentText(text);
-        nBuilder.setSmallIcon(icon);
 
-    }
+            this.context = context;
 
-    public void sendNotification() {
+            this.soundAlert = soundAlert;
+            this.startingIntent = startingIntent;
+            this.vibrate = vibrate;
 
-        Intent intent = new Intent();
-        PendingIntent pIntent = PendingIntent.getActivity(this.context, 0, intent, 0);
-        nBuilder.setContentIntent(pIntent);
+            nBuilder = new NotificationCompat.Builder(this.context);
 
-        Notification notification = nBuilder.build();
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
+            nBuilder.setTicker(tickerTitle);
+            nBuilder.setContentTitle(title);
+            nBuilder.setContentText(text);
+            nBuilder.setSmallIcon(icon);
 
-        NotificationManager notificationManager = (NotificationManager) this.context.getSystemService(this.context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notification);
+            nBuilder.setAutoCancel(autoCancel);
 
-    }
+            try {
+                nBuilder.setSound(this.soundAlert);
+            }
+            catch( NullPointerException e) {
+                this.soundFlag = false;
+            }
+        /*
+        try {
+            nBuilder.setVibrate(new long[] { 1000 });
+        }
+        catch (NullPointerException e) {
+            //Do Nothing
+        }
+        */
+
+
+        }
+
+        public void sendNotification() {
+
+            if(this.startingIntent == null) {
+                Intent intent = new Intent();
+                PendingIntent pIntent = PendingIntent.getActivity(this.context, 0, intent, 0);
+                nBuilder.setContentIntent(pIntent);
+            }
+            else {
+                PendingIntent pIntent = PendingIntent.getActivity(this.context, 0, startingIntent, 0);
+                nBuilder.setContentIntent(pIntent);
+            }
+
+            Notification notification = nBuilder.build();
+
+            if(!this.soundFlag) {
+                notification.defaults = Notification.DEFAULT_SOUND;
+            }
+        /*
+        if(this.vibrate) {
+            notification.defaults = Notification.DEFAULT_VIBRATE;
+        }
+        */
+            NotificationManager notificationManager = (NotificationManager) this.context.getSystemService(this.context.NOTIFICATION_SERVICE);
+            notificationManager.notify(0, notification);
+
+        }
 }
