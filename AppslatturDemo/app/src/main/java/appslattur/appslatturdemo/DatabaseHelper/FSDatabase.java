97b8c367bef35b6e7c149fd8eea8e7f96b23cb39
ArnJonsson <arj26@hi.is>
2015-04-03 14:31:27 +0000

    Added iterable object for gps-coord toupling

--- AppslatturDemo/app/src/main/java/appslattur/appslatturdemo/DatabaseHelper/FSDatabase.java
+++ AppslatturDemo/app/src/main/java/appslattur/appslatturdemo/DatabaseHelper/FSDatabase.java
@@ -1,35 +1,38 @@
 package appslattur.appslatturdemo.DatabaseHelper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+import appslattur.appslatturdemo.RadarHandler.RadarScannerIterable;
 
 /**
  * Created by arnarjons on 2.4.2015.
  */
 public class FSDatabase extends SQLiteOpenHelper {
 
-    public static final String DATABAS_NAME = "FSDatabase.db";
-    public static final String DATABASE_MAIN_TABLE = "StudentCard";
+    private static final String DATABAS_NAME = "FSDatabase.db";
+    private static final String DATABASE_MAIN_TABLE = "StudentCard";
 
-    public static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
 
     public FSDatabase(Context context) {
         super(context, DATABAS_NAME, null, DATABASE_VERSION);
+        this.db = this.getWritableDatabase();
     }
 
     public void onCreate(SQLiteDatabase db) {
         String newTable =
                 "CREATE TABLE IF NOT EXISTS " + DATABASE_MAIN_TABLE + " (" +
                         "ID INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                        "NAME VARCHAR NOT NULL, "   +
                         "LATITUDE TEXT NOT NULL, "  +
                         "LONGITUDE TEXT NOT NULL, " +
-                        "ENABLED INTEGER NOT NULL, "   +
-                        "SHORTDESC TEXT NOT NULL, " +
-                        "LONGDESC TEXT NOT NULL, "  +
-                        "DISCOUNT TEXT"             +
-                        "GROUP TEXT"                + ");";
+                        "ENABLED INTEGER NOT NULL, "+   ");";
         db.execSQL(newTable);
     }
 
@@ -39,5 +42,17 @@ public class FSDatabase extends SQLiteOpenHelper {
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addEntry(FSDatabaseEntry entry) {
+        ContentValues values = new ContentValues();
+        values.put("LATITUDE", entry.getLatitude());
+        values.put("LONGITUDE", entry.getLongitude());
+        values.put("ENABLED", entry.getEnableStatus());
+        this.db.insert(this.DATABASE_MAIN_TABLE, null, values);
+    }
+
+    public ArrayList<RadarScannerIterable> getIterableArrayList() {
+        return null;
     }
 }
